5ef1e8f8eb217e8a556547b3e651e4a86862eab1
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-12-02 14:42:43 +0200

    update5

--- script/admin.js
+++ script/admin.js
@@ -40,3 +40,42 @@ function adminContent(){
 }
 
 adminContent()
+
+
+function updateProduct(item){
+    try{
+        this.id = id;
+        this.make = document.querySelector('[#admin-make${item.id}]').value
+        this.spec = document.querySelector('[#admin-spec${item.id}]').value
+        this.amount = document.querySelector('[#admin-amount${item.id}]').value
+        this.action = document.querySelector('[#admin-action${item.id}]').value
+        this.image = document.querySelector('[#admin-image${item.id}]').value
+
+        let itemindex = products.findIndex((data)=>{
+            return data.id ===item.id
+        })
+
+        console.log(itemindex);
+        console.log(this);
+
+        products(itemindex) = Object.assign({}, this);
+        localStorage.setItem('products',JSON.stringify(products))
+        console.log(products);
+        adminContent()
+        location.reload()
+    }catch{}
+}
+
+
+
+
+
+function deleteProduct(){
+    try{
+        let index = products.findIndex(a =>{
+            return a.id == item.id
+        })
+        products.splice(index, 1)
+        localStorage.setItem('products', JSON.stringify(products))
+    }
+}
