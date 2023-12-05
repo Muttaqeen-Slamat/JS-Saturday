bee6f9f353e57279a1ce0265d3e4a0c272d1a38a
Muttaqeen-Slamat <muttaqeens@gmail.com>
2023-12-05 13:57:48 +0200

    patch

--- script/product.js
+++ script/product.js
@@ -71,3 +71,22 @@ searchProduct.addEventListener('keyup', ()=>{
 })
 
 
+
+
+function sorting() {
+    products.sort((a, b) => {
+        const nameA = a.name.toLowerCase();
+        const nameB = b.name.toLowerCase();
+
+        if (nameA < nameB) {
+            return -1;
+        }
+        if (nameA > nameB) {
+            return 1;
+        }
+        return 0;
+    });
+
+    displayProducts();
+}
+
